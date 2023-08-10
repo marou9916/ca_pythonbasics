7ddc7f61816e880ddb60651fb6ab36418b4e86a1
marou9916 <oumar.ndiaye447@gmail.com>
2023-08-10 13:06:06 +0200

    Conditions

---
+++ BasicStringOperations.py
@@ -0,0 +1,6 @@
+s = "Hey there ! What should this string be ?"
+#just a little practice on String characters
+print ("The number of i into the string is %d" % s.count("i"))
+print ("Its length is %s" % len(s))
+print ("The print from the 1st letter to the 6th (actually the 7th if we are strict) one gives you: %s" % s[:7])
+print ("If we rewrite the print with only uppercases : %s" % s.upper())
